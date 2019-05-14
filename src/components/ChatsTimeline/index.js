5f743c94f4cb3a4fa4ef929b449efeb975f8f603
Caspar Mac Book <casparss@users.noreply.github.com>
2019-05-14 21:25:54 +0200

    removed semis

--- src/components/ChatsTimeline/index.js
+++ src/components/ChatsTimeline/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import Chip from '@material-ui/core/Chip';
+import React from 'react'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Avatar from '@material-ui/core/Avatar'
+import Chip from '@material-ui/core/Chip'
 import './Style.scss'
 
 const stub = [{
